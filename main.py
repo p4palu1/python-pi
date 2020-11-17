1e238d0f74daf3522873edb7c41c2fe0d01d5812
p4palu1 <reem.stamker@gmail.com>
2020-11-17 19:38:03 +0200

    decimalbinary

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+# binary converter
+
+def DEC_to_BIN_converter(decnum):
+    binnum = ""
+    while decnum > 0:
+        binnum = str(decnum % 2) + binnum
+        decnum = int(decnum/2)
+    return binnum
+
+
+print(DEC_to_BIN_converter(4))
